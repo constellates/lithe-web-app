aeddb12550f17900542c667d95b89ea7003f949e
zack <zack@squishymedia.com>
2015-02-01 21:40:36 -0800

    moved css to scss based gulp flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp       = require('gulp'),
     browserify = require('gulp-browserify'),
+    sass       = require('gulp-sass'),
     concat     = require('gulp-concat');
 
 gulp.task('browserify', function () {
@@ -9,12 +10,20 @@ gulp.task('browserify', function () {
 	.pipe(gulp.dest('dist'));
 });
 
+gulp.task('sass', function () {
+    gulp.src('./src/css/main.scss')
+        .pipe(sass({
+        	outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('copy', function () {
 	gulp.src('src/index.html')
 	.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['browserify', 'copy']);
+gulp.task('default', ['browserify', 'sass', 'copy']);
 
 gulp.task('watch', function () {
 	gulp.watch('src/**/*.*', ['default']);
