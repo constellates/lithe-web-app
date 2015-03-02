0a7c589ddbebe29dd3749cd841f5184196b89b60
zack <zack@squishymedia.com>
2015-03-02 09:03:38 -0800

    reversed issue return order

--- src/js/components/issue-list.component.js
+++ src/js/components/issue-list.component.js
@@ -14,7 +14,7 @@ var IssueList = React.createClass({
 		var url = 'http://www.constellates.com:8888/issue';
 		var self = this;
 		request.get(url, function (res) {
-			self.setState({issues: res.body});
+			self.setState({issues: res.body.reverse()});
 		});
 		var issueNodes = issues.map(function (issue) {
 			return (
