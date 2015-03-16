a5b41b5f9a00cbfc290e4e40550879829ff30f98
zack <zack@squishymedia.com>
2015-03-15 19:59:19 -0700

    gave home screen basic styles hooked up mock signin form

--- src/js/components/forms/signin.component.js
+++ src/js/components/forms/signin.component.js
@@ -3,16 +3,24 @@ var request = require('superagent');
 var Text = require('../forms/text.component');
 var Password = require('../forms/password.component');
 var LoadButton = require('../forms/load-button.component.js');
+var Router = require('react-router');
 
 var SignInForm = React.createClass({
+	mixins: [Router.Navigation, Router.State],
 	render: function () {
 		return (
 			<form>
 				<Text placeholder="username" />
 				<Password placeholder="password" />
-	    		<LoadButton text="sign in" />
+	    		<LoadButton onClick={this.handleSubmit} text="sign in" />
 			</form>
     	);
+	},
+	handleSubmit: function () {
+		var self = this;
+		setTimeout(function(){
+			self.transitionTo('issues');
+		}, 3000);
 	}
 });
 
--- src/js/components/views/home.component.js
+++ src/js/components/views/home.component.js
@@ -1,23 +1,18 @@
 var React = require('react');
-var request = require('superagent');
-var Text = require('../forms/text.component');
-var Password = require('../forms/password.component');
-var LoadButton = require('../forms/load-button.component.js');
+var SignInForm = require('../forms/signin.component.js');
 
 var Home = React.createClass({
 	render: function () {
 		return (
 			<div className="home">
     			<img src={"assets/logo.svg"} />
+    			<SignInForm />
     			<h1>{'Lithe'}</h1>
     		</div>
     	);
 	},
 	handleSubmit: function () {
-		var url = 'http://www.reddit.com/.json';
-		request.get(url, function (res) {
-			console.log(res);
-		});
+
 	}
 });
 
